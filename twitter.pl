499d11e59357419584bd38317a9e79d870f1b655
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2008-05-01 17:32:09 +0000

    Suporte a SSL, TO DO list, ajuda melhor, etc.
    git-svn-id: file:///var/svn/kildclient-plugins@3 67b4d26f-7529-4ff9-a67b-0a263d19a8d0

--- twitter.pl
+++ twitter.pl
@@ -1,21 +1,39 @@
 package twitter;
-#: Version: 0.0.1
-#: Description: Cliente do Twitter para o KildClient
+#: Version: 0.0.2
+#: Description: Updates your Twitter status from KildClient
 #: Author: Alexandre "dermeister" Erwin Ittner
 #-*- coding: utf-8 -*-
 
-# Licença: GPLv2
-# CUIDADO! Software em seus primeiros estágios de desenvolvimento!
+# ---------------------------------------------------------------------------
+#
+# This program is distributed under the terms of the GNU General Public
+# License version 2 or, at your option, any later version of the license.
+# THIS PROGRAM HAVE NO WARRANTY OF ANY KIND. USE AT YOUR OWN RISK!
+#
+#
+# TO DO List:
+#   * Show 'KildClient' as source.
+#   * Interpret return messages.
+#   * Asynchronous networking to avoid freezing KC.
+#   * Remember passwords (Needs some security. Gnome Keyring?)
+#   * Capture and send game-related events (needs asynchronous calls).
+#   * Support more protocol methods.
+#   * Turn Twitter into something useful.
+#
+# ---------------------------------------------------------------------------
 
 use LWP::UserAgent;
 
+$proto = 'http://';
+$proto = 'https://' if eval("use Crypt::SSLeay;");
 
-# Configuração --------------------------------------------------------------
+# Configuration -------------------------------------------------------------
 
-our $tw_status_url = 'http://twitter.com/statuses/update.xml';
-our $tw_auth_realm = 'Twitter API';
-our $tw_auth_host  = 'twitter.com:80';
-our $tw_source     = 'KildClient';
+our $tw_status_url  = $proto . 'twitter.com/statuses/update.xml';
+our $tw_auth_realm  = 'Twitter API';
+our $tw_auth_host   = 'twitter.com:80';
+our $tw_source      = 'KildClient';
+our $user_agent     = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.14) Gecko/20080418';
 
 
 # ---------------------------------------------------------------------------
@@ -29,20 +47,26 @@ $::world->alias('^\/twstatus +(.+)$', '/twitter::status("$1")',
     { name => 'twitter:status' });
 
 sub help {
-    $::world->echonl("Plugin para usar o Twitter com o KildClient",
-        "",
-        "Comandos disponíveis:",
-        "/twauth <login> <senha>    Autentica o cliente (login não pode ter espaços)",
-        "/twstatus <status>         Atualiza seu status no twitter",
-        ""
-    )
+    $::world->echonl(<<_HELP_
+Updates your Twitter status from within KildClient. 
+
+Commands:
+    /twauth <login> <password>  Enables the plugin and stores your Twitter
+                                credentials. To allow passwords beginning with
+                                a space, login and password must be separated
+                                by _exactly one_ space.
+
+    /twstatus <status>          Sets your Twitter status to <status>.
+
+_HELP_
+);
 }
 
 sub auth {
     ($user, $pass) = @_;
     $agent = LWP::UserAgent->new();
     $agent->credentials($tw_auth_host, $tw_auth_realm, $user, $pass);
-    $agent->agent('Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.14) Gecko/20080418');
+    $agent->agent($user_agent);
     $agent->env_proxy();
     $::world->echonl("Usuário autenticado (ou não...)");
 }
@@ -58,7 +82,5 @@ sub status {
         [ status => $status, source => $tw_source ]);
     $::world->echonl("Resultado: " . $res->code . " " . $res->message);
 }
-
-
 
 
