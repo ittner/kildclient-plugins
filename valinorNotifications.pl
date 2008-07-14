e14b9d8d53dfb5b5bb1d39c82df924d54c6d1756
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2008-07-14 01:18:09 +0000

    Suporte a ícones e desabilitação de acordo com o fooc da janela principal
    git-svn-id: file:///var/svn/kildclient-plugins@10 67b4d26f-7529-4ff9-a67b-0a263d19a8d0

--- valinorNotifications.pl
+++ valinorNotifications.pl
@@ -1,33 +1,76 @@
 package valinorNotifications;
-#: Version: 0.0.1
+#: Version: 0.0.2
 #: Description: Alertas de eventos do MUD Valinor
 #: Author: Alexandre Erwin Ittner
+
+
+our $defaultTimeout = 5000;
+our $enabled = undef;
+our $basedir = $ENV{HOME} . "/.kildclient/plugins";
+
+# GPL icons copyrighted by the Pidgin project. 
+our %msgIcons = (
+    'login'   => "$basedir/log-in.png",
+    'logout'  => "$basedir/log-out.png",
+    'chat'    => "$basedir/chat.png",
+    'tell'    => "$basedir/chat.png",
+    'mtalk'   => "$basedir/chat.png"
+);
+
 
 
 BEGIN {
   $::world->requireplugin('notify');
 }
 
-$::world->trigger('^\(OOC\) (.*)', '/notify::msgSimple("chat", "MUD Valinor - Chat", $_[1], 5000)',
-    { name => 'valinorNotifications:chat' });
 
-$::world->trigger('^([A-Za-z]+\>.*)', '/notify::msgSimple("mtalk", "MUD Valinor - MTalk", $_[1], 5000)', 
-    { name => "valinorNotifications:mtalk" });
+$::world->hook('OnGetFocus', '/valinorNotifications::toggle(0)',
+    { name => 'valinorNotifications:toggle' });
 
-$::world->trigger('^\(OOC\) (.*telepaticamente.*)', '/notify::msgSimple("tell", "MUD Valinor - Tell", $_[1], 5000)', 
+$::world->hook('OnLoseFocus', '/valinorNotifications::toggle(1)',
+    { name => 'valinorNotifications:toggle' });
+
+$::world->trigger('^\(OOC\) (.*telepaticamente.*)',
+    '/valinorNotifications::message("tell", "Tell", $_[1])', 
     { name => 'valinorNotifications:tell' });
 
-$::world->trigger('^Comm: ([A-Za-z]*)@([0-9.]+) has connected.',
-    '/notify::msgSimple("tell", "MUD Valinor - Conexão", "$_[1] conectou-se de $_[2]", 5000)', 
-    { name => 'valinorNotifications:connection' });
+$::world->trigger('^\(OOC\) (.*)',
+    '/valinorNotifications::message("chat", "Chat", $_[1])',
+    { name => 'valinorNotifications:chat' });
 
-$::world->trigger('^Comm: ([A-Za-z]*)@([0-9.]+) has quit.',
-    '/notify::msgSimple("tell", "MUD Valinor - Desconexão", "$_[1] saiu", 5000)', 
-    { name => 'valinorNotifications:connection' });
+$::world->trigger('^([A-Za-z]+[\)\>].*)',
+    '/valinorNotifications::message("mtalk", "MTalk", $_[1])', 
+    { name => "valinorNotifications:mtalk" });
+
+$::world->trigger('^Comm: ([A-Za-z]*)@([0-9.]+)\(.*has connected\.',
+    '/valinorNotifications::message("login", "Conexão", "$_[1] conectou-se de $_[2]")', 
+    { name => 'valinorNotifications:login' });
+
+$::world->trigger('^Comm: ([A-Za-z]*)@([0-9.]+) has quit',
+    '/valinorNotifications::message("logout", "Desconexão", "$_[1] saiu")', 
+    { name => 'valinorNotifications:logout' });
 
 sub help {
     $::world->echonl("Alertas para uma série de eventos do MUD Valinor.");
 }
 
+sub toggle {
+    my ($status) = @_;
+    if ($status == 1) {
+        $enabled = 1;
+        notify::msgFormatted('status', "MUD Valinor - Alertas ativados",
+            "Eventos: <b>todos</b> (não há esse controle ainda)", 1500);
+    } else {
+        $enabled = undef;
+    }
+}
+
+sub message {
+    my ($event, $title, $text) = @_;
+    if (defined($enabled)) {
+        notify::msgSimple($event, "MUD Valinor - $title", $text,
+            $defaultTimeout, $msgIcons{$event});
+    }
+}
 
 
