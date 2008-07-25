cda3572c08e5feb729d8fc84259579a522f10377
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2008-07-25 23:13:59 +0000

    Verificando de o Rhythmbox está em execução e se há uma faixa tocando antes de prosseguir.
    git-svn-id: file:///var/svn/kildclient-plugins@12 67b4d26f-7529-4ff9-a67b-0a263d19a8d0

--- rhythmbox.pl
+++ rhythmbox.pl
@@ -12,7 +12,7 @@ $::world->alias('^\/music$', '/rhythmbox::sendtrack');
 $::world->alias('^\/music *(.+)$', '/rhythmbox::sendtrack("$1")');
 
 sub help {
-    $::world->send("Sends information on music tracks to the MUD.\n",
+    $::world->echo("Sends information on music tracks to the MUD.\n",
         "Commands:\n\n",
         "  /music          - Sends the track using the command '$chatcmd'\n",
         "  /music command  - Sends the track using the given command\n",
@@ -24,13 +24,36 @@ sub sendtrack {
     if (!defined($cmd)) { $cmd = $chatcmd; }
     
     my $bus = Net::DBus->session();
+    
+    my $dbus_srv = $bus->get_service("org.freedesktop.DBus");
+    my $dbus_obj = $dbus_srv->get_object("/org/freedesktop/DBus", "org.freedesktop.DBus");
+    my $running_services =  $dbus_obj->ListNames();
+    my $ok = undef;
+    
+    foreach my $tmp (@{$running_services}) {
+        if ($tmp eq "org.gnome.Rhythmbox") {
+            $ok = 1;
+            last;
+        }
+    }
+
+    if ($ok != 1) {
+        $::world->echonl("O Rhythmbox não está em execução.");        
+        return;
+    }
+    
     my $rhy = $bus->get_service("org.gnome.Rhythmbox");
     my $player = $rhy->get_object("/org/gnome/Rhythmbox/Player", "org.gnome.Rhythmbox.Player");
     my $shell = $rhy->get_object("/org/gnome/Rhythmbox/Shell", "org.gnome.Rhythmbox.Shell");
     my $url = $player->getPlayingUri;
+
+    if ($url eq "") {
+        $::world->echonl("Não há nenhuma faixa em execução.");        
+        return;
+    }   
+    
     my $songdata = $shell->getSongProperties($url);
     $::world->send($cmd . " " . $prefix . $songdata->{'artist'} . " - "
         . $songdata->{'album'} . " - " . $songdata->{'title'});
 }
-
 
