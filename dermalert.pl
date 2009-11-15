a2fad5790a0fea84fb626024ca6c9143f4ff8d02
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2009-11-15 19:17:28 +0000

    Adicionando novo plugin de alertas.
    git-svn-id: file:///var/svn/kildclient-plugins@15 67b4d26f-7529-4ff9-a67b-0a263d19a8d0

---
+++ dermalert.pl
@@ -0,0 +1,46 @@
+package dermalert;
+#: Version: 0.0.1
+#: Description: Despertador para o Dermeister
+#: Author: Alexandre Erwin Ittner
+
+
+our $defaultTimeout = 5000;
+our $enabled = undef;
+
+BEGIN {
+  $::world->requireplugin('notify');
+}
+
+
+$::world->hook('OnGetFocus', '/dermalert::toggle(0)',
+    { name => 'dermalert:toggle' });
+
+$::world->hook('OnLoseFocus', '/dermalert::toggle(1)',
+    { name => 'dermalert:toggle' });
+
+$::world->trigger('^(.*(derm|Derm|DERM).*)$',
+    '/dermalert::message("tell", "MUD chamando Dermeister!", $_[1])', 
+    { name => 'dermalert:alert', keepexecuting => 1 });
+
+
+sub help {
+    $::world->echonl("Despertador para outras atividades");
+}
+
+sub toggle {
+    my ($status) = @_;
+    if ($status == 1) {
+        $enabled = 1;
+        notify::msgSimple('status', "MUD Valinor", "Dermalert ativado!", 1500);
+    } else {
+        $enabled = undef;
+    }
+}
+
+sub message {
+    my ($event, $title, $text) = @_;
+    if (defined($enabled)) {
+        notify::msgSimple($event, $title, $text, $defaultTimeout);
+    }
+}
+
